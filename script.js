3fa1d41746da061883498c12532b272c9646ff30
andreaseh-ux <andreas_eh@hotmail.com>
2026-09-12 08:39:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 document.querySelectorAll("video").forEach(v=>{v.muted=true;v.playsInline=true;});
+if(!document.querySelector('script[src="/_vercel/insights/script.js"]')){
+  const s=document.createElement('script');
+  s.src='/_vercel/insights/script.js';
+  s.defer=true;
+  document.head.appendChild(s);
+}
